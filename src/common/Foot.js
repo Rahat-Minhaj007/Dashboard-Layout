bb9230b27915c6351bf9de79bf3fc1ef7164ba39
Rahat-Minhaj007 <rahatminhaj555@gmail.com>
2022-02-04 19:12:52 +0600

    update footer

--- src/common/Foot.js
+++ src/common/Foot.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Foot = () => (
-  <footer className=" p-2 mx-1" style={{ boxShadow: "10px 10px 40px whiteSmoke" }}>
+  <footer className=" p-2 mx-1 shadow">
     <p className="text-center text-secondary pt-3" style={{ fontSize: "10px" }}>
-      Designed, developed and maintained by minhajul abedin rahat©2021
+      Designed, developed and maintained by minhajul abedin rahat©2022
     </p>
   </footer>
 );
